5c49d1aa6743827b90d93e8118a059783d992232
Roman <vvptus@yandex.ru>
2022-11-16 00:29:56 +0300

    task

--- task27/Program.cs
+++ task27/Program.cs
@@ -1,4 +1,17 @@
-﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке 
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке
 // от M до N с помощью рекурсии.
 // M = 1; N = 5 -> "2, 4"
 // M = 4; N = 8 -> "4, 6, 8"
+
+void PrintEvenNatureNumbers(int minvalue, int maxvalue)
+{
+    if (minvalue >= maxvalue)
+    {
+        return;
+    }
+    System.Console.Write(minvalue + 1 + "  ");
+    PrintEvenNatureNumbers(minvalue + 1, maxvalue);
+    System.Console.WriteLine();
+}
+
+PrintEvenNatureNumbers(0, 10);
