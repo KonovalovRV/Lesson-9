953c26bee64ea63466d75dc9cd958f923a4e93c1
Roman <vvptus@yandex.ru>
2022-11-19 00:32:37 +0300

    task28

--- task28/Program.cs
+++ task28/Program.cs
@@ -1,4 +1,27 @@
-﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов
 // в промежутке от M до N с помощью рекурсии.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8 -> 30
+
+int Prompt(string mes)
+{
+    Console.WriteLine(mes);
+    int arg = Convert.ToInt32(Console.ReadLine());
+    return arg;
+}
+
+int SumNatureNumbers(int num1, int num2)
+{
+    if (num1 > num2)
+    {
+        return 0;
+    }
+    return num1 + SumNatureNumbers(num1 + 1, num2);
+}
+
+int minvalue=Prompt("Введите начальное значение промежутка -->   ");
+int maxvalue=Prompt("Введите конечное значение промежутка -->   ");
+
+int sum = SumNatureNumbers(minvalue, maxvalue);
+
+System.Console.WriteLine($"Сумма чисел в промежутке от {minvalue} до {maxvalue} равна {sum}");
